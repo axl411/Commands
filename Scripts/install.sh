3da26c1860fac40494ec0cecc68ae0c98ea960ed
axl411 <axl411511@live.com>
2018-12-17 15:02:50 +0900

    remove unused scripts

--- Scripts/install.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-INSTALL_DIR=$HOME/.marathon_commands
-
-mkdir -p $INSTALL_DIR
-
-marathon install xcodeOpen.swift $INSTALL_DIR/xcodeopen --force
-marathon install appcodeOpen.swift $INSTALL_DIR/appcodeopen --force
